2e15df4ca29d324c9e24e0996c97a4b82898665f
Vnywvy <bohdan05zbr@gmail.com>
2024-12-11 11:49:22 +0200

    Client: basic folder structure, start routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,25 @@
+import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
+import { Footer, Header } from "./components";
+import { Home } from "./pages";
+
+const Layout = () => (
+  <>
+    <Header />
+    <Outlet />
+    <Footer />
+  </>
+);
+
 const App = () => {
-  return <div className="App">App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
